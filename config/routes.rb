41a7cb6a61d61e1d0425853cd92e815ccdd5946a
Orban Botond <orban@toptal.com>
2020-06-09 23:23:00 +0300

    Add root redirect to investments

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :investments do
     collection do
       get 'index2'
-      get 'index3'
     end
   end
+
+  root to: redirect('investments')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
